734f143ae53aed47bc3e0ad18aa582c2d0f582de
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-05 10:52:10 -0500

    switched to async await  404 shows for adding photo because theres not index template page  make that now

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -2,23 +2,35 @@ const express = require('express')
 const router = express.Router()
 const Photo = require('../models/photo')
 
-router.get('/', (req, res) => {
-	res.send("photo controller working")
+// router.get('/', (req, res) => {
+// 	res.send("photo controller working")
+// })
+
+router.get('/', async (req, res, next) => {
+	try {
+		const foundPhotos = await Photo.find({})
+		console.log("\nfound photos:", foundPhotos);
+		res.render('photos/index.ejs', {
+			photos: foundPhotos
+		})
+	} catch(err) {
+		next(err)
+	}
 })
 
 router.get('/new', (req, res) => {
 	res.render('photos/new.ejs')
 })
 
-router.post('/', (req, res, next) => {
-	Photo.create(req.body, (err, createdPhoto) => {
-		if(err) next(err) 
-			else {
-				console.log("\ncreated photo:", createdPhoto);
-				res.send('hitting the POST ROUTE -- terminal')
-			}
+router.post('/', async (req, res, next) => {
+	try {
+		const createdPhoto = await Photo.create(req.body)
+		console.log("\ncreated photo:", createdPhoto);
+		res.redirect('/photos/' + createdPhoto.id)
+		} catch(err) {
+			next(err)
+		}
 	})
-})
 
 // router.post('/', async (req, res, next) => {
 // 	try {
