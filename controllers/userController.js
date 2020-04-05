201d25faf84390fd37c0eff3381d208358221846
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-05 12:06:28 -0500

    new photographer page working with dropdown for photos to add

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const Photo = require('../models/photo')
 
-router.get('/new', (req, res) => {
-	// res.send('user route finally')
-	res.render('users/new.ejs')
+router.get('/new', async (req, res) => {
+	try {
+		const foundPhotosForUser = await Photo.find({})
+		console.log("\nfound photos for user:", foundPhotosForUser);
+		res.render('users/new.ejs', {
+			photos: foundPhotosForUser
+		})
+	} catch(err) {
+		next(err)
+	}
 })
 
 
@@ -13,3 +21,5 @@ router.get('/new', (req, res) => {
 
 
 module.exports = router
+
+
