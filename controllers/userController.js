58df1356bd9c5598117c4efd6eca70bf782d7cc6
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-05 11:56:31 -0500

    photographer or user page made and rendering with form

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
-	res.send('user route finally')
+	// res.send('user route finally')
+	res.render('users/new.ejs')
 })
 
 
